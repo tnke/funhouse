5a81b726a4cd437ca48972a8f5c0ff4eec78ca74
Timo Sundvik <timo.sundvik@gmail.com>
2021-03-03 10:33:16 +0200

    Added revalidation and stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,5 +43,9 @@ export async function getStaticProps() {
 
   return {
     props: { allPosts },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every minute
+    revalidate: 60, // In seconds
   };
 }
